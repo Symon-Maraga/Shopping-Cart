ae247c32867c458c5548d63de0c355ae0bf543b8
Symon-Maraga <Symon-Maraga@users.noreply.github.com>
2017-09-20 16:45:42 +0300

    add

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
-from flask import flask
+import os
+from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
+app.secret_key = os.urandom(24)
 
 from app import views
+
+
 
 app.config.from_object('config')
--- app/views.py
+++ app/views.py
@@ -1,13 +1,29 @@
 #views.py
 
-from flask import render_template
-
+from flask import render_template, session
 from app import app
+# @app.route('/')
+# def index():
+# 	return render_template("login.html")
 
-@app.route('/')
+# @app.route('/home')
+# def home():
+# 	return render_template("dashboard.html")
+
+
+@app.route('/login')
 def index():
-	return render_template("login.html")
+	session['user'] = 'Symon'
+	return render_template('Login.html')
 
-@app.route('/home')
-def home():
-	return render_template("dashboard.html")
+@app.route('/getsession')
+def getsession():
+	if 'user' in session:
+		return sesion['user']
+
+	return 'Not logged in'
+	
+@app.route('/dropsession')
+def dropsession():
+	session.pop('user', none)
+	return 'Dropped!'
