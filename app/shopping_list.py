718aa5fce5fb13c4cfbfb83a57f5b69d6401080f
Symon-Maraga <Symon-Maraga@users.noreply.github.com>
2017-09-22 07:04:06 +0300

    add shopping_list

---
+++ app/shopping_list.py
@@ -0,0 +1,6 @@
+class Shoppin_list:
+    def __init__(self, fullname, username, email=None, password=None,):
+        self.fullname = fullname
+        self.username = username
+        self.email = email
+        self.password = password
